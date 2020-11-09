97287e40703a071963862873745695c27f25990a
Syed Waqar Uddin <uddin.8@wright.edu>
2020-11-08 20:55:14 -0500

    Added comments in code

--- app/src/test/java/com/example/settesting/MySetTest.java
+++ app/src/test/java/com/example/settesting/MySetTest.java
@@ -3,13 +3,24 @@ package com.example.settesting;
 import org.junit.Assert;
 import org.junit.Test;
 
+
+//requirement
+//Do your own Set implementation (using arrays) with TDD pair programming
+//Set bag of elements
+//add elements
+//add method to add
+
+//todo
+//should not contain duplicates
+//add should return false and should not add element into array
+
 public class MySetTest {
 
     @Test
-    public void firstTest(){
+    public void firstTest() {
         MySet mySet = new MySet();
         mySet.add(1);
-        boolean actualValue = mySet.add(1);
+        boolean actualValue = mySet.add(1); // adding element
         boolean expectedValue = true;
 
         Assert.assertEquals(expectedValue, actualValue);
@@ -18,10 +29,10 @@ public class MySetTest {
     }
 
     @Test
-    public void secondTest(){
+    public void secondTest() {
         MySet mySet = new MySet();
         mySet.add(1);
-        boolean actualValue = mySet.contains(1);
+        boolean actualValue = mySet.contains(1); // should return true if 1 is added
         boolean expectedValue = true;
 
         Assert.assertEquals(expectedValue, actualValue);
@@ -30,10 +41,10 @@ public class MySetTest {
     }
 
     @Test // negative cases
-    public void thirdTest(){
+    public void thirdTest() {
         MySet mySet = new MySet();
         mySet.add(1);
-        boolean actualValue = mySet.contains(2);
+        boolean actualValue = mySet.contains(2); // should return false as we have not added 2
         boolean expectedValue = false;
 
         Assert.assertEquals(expectedValue, actualValue);
